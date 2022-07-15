eaaae645777f2aeae1d32724d35813650782b31e
Riccardo <riccardo.valentini@outlook.com>
2022-07-15 15:19:35 +0200

    Aggiunto SectionsPagerAdapter.kt

--- app/src/main/java/com/valentini/mypoi/ui/main/SectionsPagerAdapter.kt
+++ app/src/main/java/com/valentini/mypoi/ui/main/SectionsPagerAdapter.kt
@@ -1,14 +1,15 @@
-package com.valentini.mypoi.ui.main
+package com.example.ha.ui.main
 
 import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
-import com.valentini.mypoi.R
+import com.example.ha.R
 
 private val TAB_TITLES = arrayOf(
-    R.string.tab_text_1,
-    R.string.tab_text_2
+    R.string.tab_home,
+    R.string.tab_map,
+    R.string.tab_myplaces,
 )
 
 /**
@@ -16,20 +17,33 @@ private val TAB_TITLES = arrayOf(
  * one of the sections/tabs/pages.
  */
 class SectionsPagerAdapter(private val context: Context, fm: FragmentManager) :
-    FragmentPagerAdapter(fm) {
+    FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
 
     override fun getItem(position: Int): Fragment {
+        when (position) {
+            0 -> {
+                //Toast.makeText(context, "Sezione 1", Toast.LENGTH_SHORT).show()
+                return HomeFragment.newInstance(position + 1) //+1 perché il tab fa 0,1,2,3...
+            }
+            1 -> {
+                //Toast.makeText(context, "Sezione 2", Toast.LENGTH_SHORT).show()
+                return MapFragment.newInstance(position + 1) //+1 perché il tab fa 0,1,2,3...
+            }
+            2 -> {
+                //Toast.makeText(context, "Sezione 3", Toast.LENGTH_SHORT).show()
+                return MyPlacesFragment.newInstance(position + 1)
+            }
+        }
         // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1)
+        // Return a MyPlacesFragment (defined as a static inner class below).
+        return Fragment()
     }
 
-    override fun getPageTitle(position: Int): CharSequence? {
+    override fun getPageTitle(position: Int): CharSequence {
         return context.resources.getString(TAB_TITLES[position])
     }
 
     override fun getCount(): Int {
-        // Show 2 total pages.
-        return 2
+        return TAB_TITLES.size
     }
 }
